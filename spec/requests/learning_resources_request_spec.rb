d6aba8407b521bdc2c6102abd6c9797e27da7d7d
Sid Mann <sjmann2@outlook.com>
2022-11-11 14:48:34 -0600

    test learning resources response

--- spec/requests/learning_resources_request_spec.rb
+++ spec/requests/learning_resources_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'The learning resources request' do
   describe 'GET /api/v1/learning_resources?country=laos' do
-    xit 'returns learning resources for a given country' do
+    it 'returns learning resources for a given country' do
       get '/api/v1/learning_resources?country=laos'
 
       expect(response).to be_successful
@@ -14,7 +14,12 @@ RSpec.describe 'The learning resources request' do
       expect(resources[:data]).to have_key(:type)
       expect(resources[:data][:attributes]).to have_key(:country)
       expect(resources[:data][:attributes][:country]).to be_a(String)
-      expect(resources[:data][:attributes][:country]).to be_a(String)
+      expect(resources[:data][:attributes][:video]).to be_a(Hash)
+      expect(resources[:data][:attributes][:video][:title]).to be_a(String)
+      expect(resources[:data][:attributes][:video][:youtube_video_id]).to be_a(String)
+      expect(resources[:data][:attributes][:images]).to be_an(Array)
+      expect(resources[:data][:attributes][:images].first[:alt_tag]).to be_a(String)
+      expect(resources[:data][:attributes][:images].first[:url]).to be_a(String)
     end
   end
 end
