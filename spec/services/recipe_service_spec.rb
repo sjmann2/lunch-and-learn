9b767d2796ec01ec1015e4a55bccfc3029fc0193
Sid Mann <sjmann2@outlook.com>
2022-11-10 20:06:55 -0600

    test recipe service get recipes

---
+++ spec/services/recipe_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe RecipeService do
+  it 'returns an HTTP response of recipes given a keyword', :vcr do
+    response = RecipeService.get_recipes('thailand')
+
+    expect(response).to be_a(Hash)
+    expect(response[:hits]).to be_an(Array)
+    expect(response[:q]).to eq("thailand")
+    recipe = response[:hits].first
+    expect(recipe[:recipe]).to have_key(:uri)
+    expect(recipe[:recipe][:uri]).to be_a(String)
+    expect(recipe[:recipe][:label]).to be_a(String)
+    expect(recipe[:recipe][:image]).to be_a(String)
+  end
+end
