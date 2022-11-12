81f69936fb8b464a655e46e2efa0cd985a749b0c
Sid Mann <sjmann2@outlook.com>
2022-11-12 17:10:20 -0600

    feat happy path post a favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
-    favorite = Favorite.new(favorite_params)
+    user = User.find_by(api_key: params[:api_key])
+    favorite = user.favorites.new(favorite_params)
     if favorite.save
-      render json: FavoriteSerializer.new(favorite)
+      render json: FavoriteSerializer.post_favorite, status: 201
     else
       error = ErrorSerializer.new(favorite.errors.full_messages, 'BAD REQUEST', 400)
       render json: error.serialized_message, status: 400
     end
+  end
+
+  private
+  
+  def favorite_params
+    params.permit(:api_key, :country, :recipe_link, :recipe_title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :api_key
   validates_uniqueness_of :email, :api_key
   
-  before_validation :set_api_key
+  before_validation :set_api_key, if: :api_key_nil?
 
   private
 
   def set_api_key
     self.api_key = ApiKey.generator
   end
+
+  def api_key_nil?
+    self.api_key.nil?
+  end
+
 end
