df4cbf0ace04631dbd7b169d95106573da8ec1cd
Sid Mann <sjmann2@outlook.com>
2022-11-12 16:12:12 -0600

    update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :api_key
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :api_key
   
   before_validation :set_api_key
 
