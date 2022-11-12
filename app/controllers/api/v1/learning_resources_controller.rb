096d2d86c49f0db4406923e6cffba3c60496b0e1
Sid Mann <sjmann2@outlook.com>
2022-11-11 18:24:51 -0600

    serialize learning resources

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def index
-
+    @country = params[:country]
+    video = VideoFacade.get_video(@country)
+    photos = PhotoFacade.get_photos(@country)
+    render json: LearningResoucesSerializer.format_resources(video, photos)
   end
 end
---
+++ app/serializers/learning_resouces_serializer.rb
@@ -0,0 +1,23 @@
+class LearningResoucesSerializer
+  def self.format_resources(video, photos)
+    {
+      data: {
+        id: "null",
+        type: "learning_resource",
+        attributes: {
+          country: video.country,
+          video: {
+            title: video.title,
+            youtube_video_id: video.youtube_video_id
+          },
+          images: photos.map do |photo|
+            {
+              alt_tag: photo.alt_tag,
+              url: photo.url
+            }
+          end
+        }
+      }
+    }
+  end
+end
