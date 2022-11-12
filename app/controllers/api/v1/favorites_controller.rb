6c4905b0a15fccd720b6405d42195ded6d3c93c0
Sid Mann <sjmann2@outlook.com>
2022-11-12 17:36:36 -0600

    test/feat sad path api key does not match any user

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
+    if user.nil?
+      error = ErrorSerializer.new("No user found with api_key #{params[:api_key]}", 'NOT FOUND', 404)
+      render json: error.serialized_message, status: 404
+      return
+    end
     favorite = user.favorites.new(favorite_params)
     if favorite.save
       render json: FavoriteSerializer.post_favorite, status: 201
--- spec/requests/favorite_request_spec.rb
+++ spec/requests/favorite_request_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'The favorite request' do
         body = JSON.generate(
           api_key: "jgn983hy48thw9begh98h4539h4",
           country: "thailand",
-          recipe_link: "https://www.tastingtable.com/.....", 
+          recipe_link: "https://www.tastingtable.com", 
           recipe_title: "Crab Fried Rice (Khaao Pad Bpu)")
 
         post '/api/v1/favorites', headers: headers, params: body
@@ -22,6 +22,25 @@ RSpec.describe 'The favorite request' do
 
         expect(result).to have_key(:success)
         expect(result[:success]).to eq("Favorite added successfully")
+      end
+    end
+
+    describe 'When the api key does not match any user' do
+      it 'returns a 400 status and error message' do
+        user = User.create!(name: "Tina", email: "linagirl@yahoo.com", api_key: 'jgn983hy48thw9begh98h4539h4')
+        
+        headers = {'CONTENT_TYPE' => 'application/json'}
+        body = JSON.generate(
+          api_key: "knm483hy48thw9begh98h4539h7",
+          country: "thailand",
+          recipe_link: "https://www.tastingtable.com", 
+          recipe_title: "Crab Fried Rice (Khaao Pad Bpu)")
+
+        post '/api/v1/favorites', headers: headers, params: body
+
+        expect(response).to have_http_status(404)
+        result = JSON.parse(response.body, symbolize_names: true)
+        expect(result[:errors].first[:message]).to eq("No user found with api_key knm483hy48thw9begh98h4539h7")
       end
     end
   end
