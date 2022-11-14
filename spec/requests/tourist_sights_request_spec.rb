899a54003fe01649898e068f95937a2380b24bd5
Sid Mann <sjmann2@outlook.com>
2022-11-14 11:41:05 -0600

    test sad path if no sites are found for a country

--- spec/requests/tourist_sights_request_spec.rb
+++ spec/requests/tourist_sights_request_spec.rb
@@ -2,29 +2,43 @@ require 'rails_helper'
 
 RSpec.describe 'The tourist sights request' do
   describe 'GET /api/v1/tourist_sights?country=France' do
-    it 'returns a collection of all tourist sites within a 20000 meter radius of the capital city', :vcr do
-      get '/api/v1/tourist_sights?country=France'
+    describe 'When the country has tourist sites within a 20000 meter radius of the capital' do
+      it 'returns a collection of all tourist sites within a 20000 meter radius of the capital city', :vcr do
+        get '/api/v1/tourist_sights?country=France'
 
-      expect(response).to be_successful
-      expect(response).to have_http_status(200)
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
 
-      sites = JSON.parse(response.body, symbolize_names: true)
+        sites = JSON.parse(response.body, symbolize_names: true)
 
-      expect(sites).to be_a(Hash)
-      expect(sites[:data]).to be_an(Array)
+        expect(sites).to be_a(Hash)
+        expect(sites[:data]).to be_an(Array)
 
-      site = sites[:data].first
+        site = sites[:data].first
 
-      expect(site).to have_key(:id)
-      expect(site).to have_key(:type)
-      expect(site).to have_key(:attributes)
-      expect(site[:attributes].count).to eq(3)
-      expect(site[:attributes]).to have_key(:name)
-      expect(site[:attributes][:name]).to be_a(String)
-      expect(site[:attributes]).to have_key(:address)
-      expect(site[:attributes][:address]).to be_a(String)
-      expect(site[:attributes]).to have_key(:place_id)
-      expect(site[:attributes][:place_id]).to be_a(String)
+        expect(site).to have_key(:id)
+        expect(site).to have_key(:type)
+        expect(site).to have_key(:attributes)
+        expect(site[:attributes].count).to eq(3)
+        expect(site[:attributes]).to have_key(:name)
+        expect(site[:attributes][:name]).to be_a(String)
+        expect(site[:attributes]).to have_key(:address)
+        expect(site[:attributes][:address]).to be_a(String)
+        expect(site[:attributes]).to have_key(:place_id)
+        expect(site[:attributes][:place_id]).to be_a(String)
+      end
+    end
+
+    describe 'When the country does not have tourist sites within a 20000 meter radius of the capital' do
+      it 'returns a response with an empty body', :vcr do
+        get '/api/v1/tourist_sights?country=Ecuador'
+
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+
+        sites = JSON.parse(response.body, symbolize_names: true)
+        expect(sites[:data]).to be_empty
+      end
     end
   end
 end
