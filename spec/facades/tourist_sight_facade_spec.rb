32942d2609bff6fff4049c3fd8dd22c3d7d2ee88
Sid Mann <sjmann2@outlook.com>
2022-11-14 13:07:13 -0600

    update facade test to two word country

--- spec/facades/tourist_sight_facade_spec.rb
+++ spec/facades/tourist_sight_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TouristSightFacade do
   it 'returns tourist sight objects for the given country', :vcr do
-    response = TouristSightFacade.get_sights('France')
+    response = TouristSightFacade.get_sights('New zealand')
 
     expect(response).to be_an(Array)
     expect(response.first).to be_a(TouristSight)
