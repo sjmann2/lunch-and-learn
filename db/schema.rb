0393f35fe3619f68ef5cd9adb85226f48e15e6ef
Sid Mann <sjmann2@outlook.com>
2022-11-15 09:55:33 -0600

    Add password to user, enable bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_12_223640) do
+ActiveRecord::Schema.define(version: 2022_11_15_155123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_11_12_223640) do
     t.string "name"
     t.string "email"
     t.string "api_key"
+    t.string "password_digest"
   end
 
   add_foreign_key "favorites", "users"
